7094209eea7e9f26ad5f8c92cafce27e6a5a2aea
Oana Topan <91469778+TopanOana@users.noreply.github.com>
2024-03-18 11:05:21 +0200

    alta incercare

--- src/test/java/ssvv/example/AppTest.java
+++ src/test/java/ssvv/example/AppTest.java
@@ -1,9 +1,8 @@
 package ssvv.example;
-//
-//import junit.framework.Test;
-//import junit.framework.TestSuite;
+
+
 import junit.framework.TestSuite;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import ssvv.example.domain.Student;
 import ssvv.example.repository.StudentRepository;
 import ssvv.example.validation.StudentValidator;
@@ -13,8 +12,7 @@ import static junit.framework.Assert.*;
 /**
  * Unit test for simple App.
  */
-public class AppTest
-{
+public class AppTest {
     /**
      * Create the test case
      *
@@ -24,48 +22,45 @@ public class AppTest
     public StudentRepository studentRepository;
 
     public Student student;
-    public AppTest(  )
-    {
+
+    public AppTest() {
 //        super( testName );
         studentRepository = new StudentRepository(new StudentValidator());
     }
 
     @Test
-    public void tc_valid_studentID(){
+    public void tc_valid_studentID() {
         student = new Student("1", "name student", 937);
-        try{
+        try {
             studentRepository.save(student);
             assertTrue(true);
-        }catch (Exception ex){
+        } catch (Exception ex) {
             fail();
         }
     }
 
     @Test
-    public void tc_invalid_studentID(){
+    public void tc_invalid_studentID() {
         student = new Student("", "name student", 937);
-        try{
-            studentRepository.save(student);
-            fail();
-        }catch (Exception ex){
+
+        if (studentRepository.save(student) == null)
             assertTrue(true);
-        }
+        else fail();
+
     }
 
 
     /**
      * @return the suite of tests being tested
      */
-    public static TestSuite suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
+//    public static Test suite() {
+//        return (Test) new TestSuite(AppTest.class);
+//    }
 
     /**
      * Rigourous Test :-)
      */
-    public void testApp()
-    {
-        assertTrue( true );
+    public void testApp() {
+        assertTrue(true);
     }
 }
